ebdb7d77b132d12c838e86eb0d3f7341fe971613
Ollie Field <oliverfield99@gmail.com>
2019-02-19 13:24:38 +0000

    Pantry table class

---
+++ app/src/main/java/database/DBPantry.java
@@ -0,0 +1,49 @@
+package database;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.Date;
+
+public class DBPantry  {
+    public static final String TABLENAME = "pantryTable";
+    public static final String COLUMN_FOODNAME = "foodName";
+    public static final String COLUMN_DATEADDED = "dateAdded";
+    public static final String COLUMN_EXPIRY = "dateExpire";
+    public static final String COLUMN_AMOUNT = "amount";
+
+    private String foodName;
+    private String dateAdded;
+    private String dateExpiry;
+    private int amount;
+
+
+    public static final String CREATE_TABLE = "CREATE TABLE " + TABLENAME + "("
+            + COLUMN_FOODNAME + " TEXT, "
+            + COLUMN_DATEADDED + " DATETIME DEFAULT CURRENT_TIMESTAMP, "
+            + COLUMN_EXPIRY + " DATETIME, "
+            + COLUMN_AMOUNT + " INT, "
+            + "PRIMARY KEY (foodName, dateAdded)" + ")";
+
+    public DBPantry(){};
+    public DBPantry (String name, String added, String expiry, int amount)
+    {
+        this.foodName = name;
+        this.dateAdded = added;
+        this.dateExpiry = expiry;
+        this.amount = amount;
+    }
+
+    public String getName() {return foodName;}
+    public void setName(String name){ this.foodName = name;}
+
+    public String getDateAdded () {return dateAdded;}
+    public void setDateAdded (String added) { this.dateAdded = added;}
+
+    public String getDateExpiry () {return dateExpiry;}
+    public void setDateExpiry(String expiry) {this.dateExpiry = expiry;}
+
+    public int getAmount () {return amount;}
+    public void setAmount (int amount) { this.amount = amount;}
+}
